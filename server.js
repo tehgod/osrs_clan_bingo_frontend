d105c55666a04bfb00ac42319d7a48bcb410b048
Shelby <shelbygt500martin@gmail.com>
2025-06-14 11:19:16 -0400

    updated images to fix cdn issue

--- server.js
+++ server.js
@@ -110,6 +110,34 @@ async function sendDiscordUpdate(webhookUrl, payload) {
     }
 }
 
+function fixcdn (urlString) {
+    const url = new URL(urlString);  // Create URL object from the string
+
+    const exStr = url.searchParams.get('ex');
+    const exTime = parseInt(exStr, 16) * 1000;
+
+    // If the expiration time is invalid or expired, redirect to CDN
+    if (isNaN(exTime) || exTime <= Date.now()) {
+        return 'https://fixcdn.hyonsu.com' + url.pathname;
+    }
+
+    // If the response code is 404, redirect to CDN
+    const navigationData = performance.getEntriesByType('navigation')[0];
+    const responseCode = navigationData ? navigationData.responseStatus : null;
+
+    if (responseCode === 404) {
+        return 'https://fixcdn.hyonsu.com' + url.pathname;
+    }
+
+    // If the page content is unavailable, redirect to CDN
+    if (document.body.innerText.trim() === 'This content is no longer available.') {
+        return 'https://fixcdn.hyonsu.com' + url.pathname;
+    }
+
+    // If everything is fine, return the original URL
+    return url.href;
+};
+
 const db = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     port: 3306,
@@ -248,8 +276,9 @@ app.post('/api/update-tile', checkSession, async (req, res) => {
     } 
 
     for (const imageUrl of imageUrls) {
+        newUrl = fixcdn(imageUrl);
         var sql = `INSERT INTO CurrentLayoutUrls(Team, Cell, Url) VALUES (?, ?, ?)`;
-        var values = [teamId, tile, imageUrl];
+        var values = [teamId, tile, newUrl];
         try {
             await queryDatabase(sql, values)
         } catch (error) {
